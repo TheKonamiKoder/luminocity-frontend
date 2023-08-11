25888d57a9f4a65f009b48ce35fa4e14f69add8e
TheKonamiKoder <thekonamicode02@gmail.com>
2023-08-11 11:52:41 +0100

    Use GetVal() to get data from SensorData

--- sensor.go
+++ sensor.go
@@ -10,12 +10,31 @@ const (
 )
 
 // LightSensorData | TemperatureSensorData | HumiditySensorData | MotionSensorData
-type SensorData interface{}
+type SensorData interface{ GetVal() interface{} }
 
 type LightSensorData struct{ Val uint8 }
+
+func (l LightSensorData) GetVal() interface{} {
+	return l.Val
+}
+
 type TemperatureSensorData struct{ Val float32 }
+
+func (t TemperatureSensorData) GetVal() interface{} {
+	return t.Val
+}
+
 type HumiditySensorData struct{ Val float32 }
+
+func (h HumiditySensorData) GetVal() interface{} {
+	return h.Val
+}
+
 type MotionSensorData struct{ Val bool }
+
+func (m MotionSensorData) GetVal() interface{} {
+	return m.Val
+}
 
 type Sensor struct {
 	Id   uint32
